46d3f75dec07ca6efe2b4d27bd0404bc7f5e1e36
Anwesh43 <anweshthecool0@gmail.com>
2020-03-28 05:45:51 +0530

    adding libraries and constants

--- steparcmoverview/src/main/java/com/anwesh/uiprojects/steparcmoverview/StepArcMoverView.kt
+++ steparcmoverview/src/main/java/com/anwesh/uiprojects/steparcmoverview/StepArcMoverView.kt
@@ -4,3 +4,21 @@ package com.anwesh.uiprojects.steparcmoverview
  * Created by anweshmishra on 28/03/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val parts : Int = 3
+val scGap : Float = 0.02f
+val delay : Long = 20
+val deg : Float = 360f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
