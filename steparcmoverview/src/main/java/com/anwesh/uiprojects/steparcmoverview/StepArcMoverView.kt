8c669f4ff44a1df7d1b7a367cd1799ef24a05ac6
Anwesh43 <anweshthecool0@gmail.com>
2020-03-28 06:06:54 +0530

    adding SAMNode class

--- steparcmoverview/src/main/java/com/anwesh/uiprojects/steparcmoverview/StepArcMoverView.kt
+++ steparcmoverview/src/main/java/com/anwesh/uiprojects/steparcmoverview/StepArcMoverView.kt
@@ -115,4 +115,45 @@ class StepArcMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAMNode(var i : Int, val state : State = State()) {
+
+        private var next : SAMNode? = null
+        private var prev : SAMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SAMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAMNode {
+            var curr : SAMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
