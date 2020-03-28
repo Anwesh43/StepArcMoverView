eb549fadbb19b68a3be8b3534b59bba7229c6139
Anwesh43 <anweshthecool0@gmail.com>
2020-03-28 06:25:36 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsteparcmoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsteparcmoverview/MainActivity.kt
@@ -2,11 +2,13 @@ package com.anwesh.uiprojects.linkedsteparcmoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.steparcmoverview.StepArcMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepArcMoverView.create(this)
     }
 }
+
